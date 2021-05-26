72c6935781a72b26bead89c9c36a165c4d64651d
Huidae Cho <geni@dream.x>
2021-05-26 02:01:59 -0400

    Make it compilable using GCC 10

--- autogen.sh
+++ autogen.sh
@@ -22,5 +22,5 @@ echo 'Running autoreconf -ifv...'
 
 if test -z "$NOCONFIGURE" ; then
 	echo 'Configuring...'
-	"${bs_dir}"/configure CFLAGS="-O2 -fomit-frame-pointer -fno-stack-protector" "$@"
+	"${bs_dir}"/configure CFLAGS="-O2 -fomit-frame-pointer -fno-stack-protector -fcommon" "$@"
 fi
